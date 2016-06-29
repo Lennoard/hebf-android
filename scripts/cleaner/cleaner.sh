0a079a7ffbae78ca28453f88419891225f1f2b2d
Lennoard Silva <lennoardrai@gmail.com>
2016-06-29 10:58:01 -0300

    Update cleaner.sh

--- scripts/cleaner/cleaner.sh
+++ scripts/cleaner/cleaner.sh
@@ -12,11 +12,8 @@
 #  You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #=======================================================================#
 
-mount -o remount,rw /
-mount -o remount,rw rootfs
 mount -o remount,rw /system
 mount -o remount,rw /data
-mount -o remount,rw /cache
 
 HEBF=/system/etc/HEBF/cleaner.log
 
@@ -54,4 +51,3 @@ HEBF=/system/etc/HEBF/cleaner.log
 
 mount -o ro,remount /system
 mount -o ro,remount /data
-mount -o ro,remount /cache
