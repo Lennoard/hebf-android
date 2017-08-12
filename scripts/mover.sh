9fc79b783a4c5b9369ab9be81d5cf4ff8c22cf67
lennoard <lennoardrai@gmail.com>
2017-08-12 11:11:36 -0300

    v2.0.0

--- scripts/mover.sh
+++ scripts/mover.sh
@@ -14,58 +14,29 @@
 mount -o rw,remount /system
 mount -o rw,remount /data
 
-busybox mkdir /data/data/com.androidvip.hebf/LMK
-busybox mkdir /data/data/com.androidvip.hebf/limpador
 busybox mkdir /data/data/com.androidvip.hebf/arquivos
-busybox mkdir /data/data/com.androidvip.hebf/hebf_logs
 
-busybox touch /data/data/com.androidvip.hebf/hebf_logs/app.log
+cp /data/data/com.androidvip.hebf/adblock /data/data/com.androidvip.hebf/arquivos/adblock
+rm -f /data/data/com.androidvip.hebf/adblock
 
-IN1=/data/data/com.androidvip.hebf/adblock
-OUT1=/data/data/com.androidvip.hebf/arquivos/adblock
-dd if="$IN1" of="$OUT1"
-rm -f "$IN1"
-
-IN2=/data/data/com.androidvip.hebf/no_adblock
-OUT2=/data/data/com.androidvip.hebf/arquivos/no_adblock
-dd if="$IN2" of="$OUT2"
-rm -f "$IN2"
+cp /data/data/com.androidvip.hebf/no_adblock /data/data/com.androidvip.hebf/arquivos/no_adblock
+rm -f /data/data/com.androidvip.hebf/no_adblock
 
 IN4=/data/data/com.androidvip.hebf/cleaner
-OUT4=/data/data/com.androidvip.hebf/limpador/cleaner
+OUT4=/data/data/com.androidvip.hebf/cleaner
 dd if="$IN4" of="$OUT4"
 rm -f "$IN4"
 
-IN5=/data/data/com.androidvip.hebf/cleaner_etc
-OUT5=/data/data/com.androidvip.hebf/limpador/cleaner_etc
-dd if="$IN5" of="$OUT5"
-rm -f "$IN5"
-
-IN7=/data/data/com.androidvip.hebf/zipalign
-OUT7=/system/bin/zipalign
-dd if="$IN7" of="$OUT7"
-rm -f "$IN7"
+cp /data/data/com.androidvip.hebf/zipalign /system/bin/zipalign
+rm -f /data/data/com.androidvip.hebf/zipalign
 busybox chmod 755 /system/bin/zipalign
 
-IN14=/data/data/com.androidvip.hebf/thumb
-OUT14=/data/data/com.androidvip.hebf/limpador/thumb
-dd if="$IN14" of="$OUT14"
-rm -f "$IN14"
-
-IN15=/data/data/com.androidvip.hebf/get_lmk
-OUT15=/data/data/com.androidvip.hebf/LMK/get_lmk
-dd if="$IN15" of="$OUT15"
-rm -f "$IN15"
-
-IN16=/data/data/com.androidvip.hebf/sqlite3
-OUT16=/system/xbin/sqlite3
-dd if="$IN16" of="$OUT16"
-rm -f "$IN16"
+cp /data/data/com.androidvip.hebf/sqlite3 /system/xbin/sqlite3
+rm -f /data/data/com.androidvip.hebf/sqlite3
 busybox chmod 755 /system/xbin/sqlite3
 
 busybox touch /data/data/com.androidvip.hebf/hebf.hebf
 busybox echo "[I] |$(date +%Y/%m/%d) $(date +%A), $(date +%H:%M:%S)| HEBF has been updated / set up" >> /data/data/com.androidvip.hebf/hebf.hebf
-busybox echo "[I] |$(date +%Y/%m/%d) $(date +%A), $(date +%H:%M:%S)| HEBF has been updated / set up" >> /data/data/com.androidvip.hebf/hebf_logs/app.log
+
 mount -o rw,remount /data
 mount -o ro,remount /system
-
