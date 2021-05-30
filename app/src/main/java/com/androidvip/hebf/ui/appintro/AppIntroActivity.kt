3f803f5d519364e34e11a351ca651091b4586c1f
Lennoard <lennoardrai@gmail.com>
2021-05-30 15:49:36 -0300

    Signing release config

--- app/src/main/java/com/androidvip/hebf/ui/appintro/AppIntroActivity.kt
+++ app/src/main/java/com/androidvip/hebf/ui/appintro/AppIntroActivity.kt
@@ -12,6 +12,7 @@ import android.widget.*
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
@@ -47,6 +48,7 @@ class AppIntroActivity : BaseActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+        ViewCompat.setElevation(toolbar, 0F)
 
         val tempFolder = K.HEBF.getTempDir(this)
         if (!tempFolder.exists()) {
